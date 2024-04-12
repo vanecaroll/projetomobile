992a6f01bda8f1a6702dc490418fdfc33ed7c9d3
vanecaroll <145063045+vanecaroll@users.noreply.github.com>
2024-04-12 18:54:23 -0300

    Add files via upload

---
+++ Menu/script.js
@@ -0,0 +1,9 @@
+function clickMenu(){
+    if (menu.style.display == 'block'){
+        menu.style.display = 'none'
+    }else {
+        menu.style.display = 'block'
+    }
+}
+
+<button class="cardapios" onclick="window.location.href='#cardapios'">VER CARDÁPIOS</button>
